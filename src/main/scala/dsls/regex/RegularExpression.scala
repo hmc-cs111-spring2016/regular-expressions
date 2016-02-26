d006ca0d490ca195f88aa76fcb915cc8d474cdd6
rjustice05 <rjustice@hmc.edu>
2016-02-25 22:15:33 -0800

    Added comments to code

--- src/main/scala/dsls/regex/RegularExpression.scala
+++ src/main/scala/dsls/regex/RegularExpression.scala
@@ -15,14 +15,19 @@ abstract class RegularExpression {
   /** returns true if the given string matches this regular expression */
 	def matches(string: String) = RegexMatcher.matches(string, this)
 
+	//The regular expression a || b describes the language {a, b}, which contains two strings
 	def || (that: RegularExpression) = Union(this,that)
 
+	// a ~ b = ab
 	def ~ (that: RegularExpression) = Concat(this,that)
 
+	// +, which means "one or more repetitions of the preceding pattern"
 	def <+> () = this ~ (this<*>)
 
+	//*, which corresponds to the Kleene star operation
 	def <*> () = Star(this)
 
+	//{n} which means "n repetitions of the preceding pattern"
 	def apply (num: Int): RegularExpression = {
 		if (num > 0) {
 			this ~ this{num-1}
@@ -35,8 +40,10 @@ abstract class RegularExpression {
 }
 
 object RegularExpression {
+	//Converts a character to a Regular Expression
 	implicit def fromChar(c: Char) = Literal(c)
 
+	//Converts a string to a Regular Expression
 	implicit def fromString(word: String): RegularExpression = {
 		if (word.equals("")) {
 			EPSILON
@@ -54,9 +61,6 @@ object EPSILON extends RegularExpression
 
 /** a regular expression that matches a literal character */
 case class Literal(val literal: Char) extends RegularExpression
-
-/** a regular expression that matches a literal string */
-//case class Literal(val literal: String) extends RegularExpression
 
 /** a regular expression that matches either one expression or another */
 case class Union(val left: RegularExpression, val right: RegularExpression) 
